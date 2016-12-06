a07af38fb9957cea7d046299363dcd72b03879e7
alejandronava13 <fismat@IMAC2.local>
2016-12-06 09:38:34 -0600

    intento de main.py

---
+++ Ejercicio 3B/main.py
@@ -0,0 +1,11 @@
+import Ejercicio 1.py as A
+import Ejercicio 2.py as B
+import Ejercicio 3.py as C
+import Ejercicio 4.py as D
+import Ejercicio 5.py as E
+A.entrega()
+B.entrega()
+C.entrega()
+D.entrega()
+E.entrega()
+F.entrega()
